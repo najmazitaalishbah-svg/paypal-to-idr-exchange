587743bdd072b248e0c515798f74a51c420049c5
zita-boop <apippudin1307@gmail.com>
2026-01-16 01:05:14 +0700

    Change orderID retrieval to use JSON input

--- api/api/api/capture-order.php
+++ api/api/api/capture-order.php
@@ -1,16 +1,19 @@
 <?php
 require 'config.php';
 
-if (!isset($_POST['orderID'])) {
+header('Content-Type: application/json');
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+if (!isset($data['orderID'])) {
     http_response_code(400);
     echo json_encode(['error' => 'Order ID is required']);
     exit;
 }
 
-$orderID = $_POST['orderID'];
+$orderID = $data['orderID'];
 
 $ch = curl_init(PAYPAL_API_BASE . "/v2/checkout/orders/$orderID/capture");
-
 curl_setopt_array($ch, [
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_POST => true,
@@ -19,6 +22,8 @@ curl_setopt_array($ch, [
 ]);
 
 $response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
+http_response_code($httpCode);
 echo $response;
